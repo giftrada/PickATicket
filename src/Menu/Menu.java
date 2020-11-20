d31d5bf02d4d553d881daa5ccac082564467bbd6
giftrada <72536596+giftrada@users.noreply.github.com>
2020-11-20 22:28:55 +0700

    Initial commit

---
+++ src/Menu/Menu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Menu;
+
+import java.util.Scanner;
+
+
+
+public class Menu {
+     public static void main(String[] args) {
+     
+     Ticket tic=new Ticket("abcde");
+     Scanner scr = new Scanner(System.in);
+     tic.Menu();
+     tic.Select(scr);
+     
+}
+}
---
+++ src/Menu/Ticket.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Menu;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author giftradakorn
+ */
+public class Ticket {
+    Scanner scr = new Scanner(System.in);    
+    private int noTicket;
+    private int choice;
+    private String[] seat = new String[100];
+    private int[] noSeat = new int[100];
+    private String nameMusical;
+    
+    public Ticket(String nameMusical) {
+        this.nameMusical = nameMusical;
+    }
+    
+    public void Menu(){
+      System.out.println("Welcome to Pick a Ticket");
+      System.out.println("---------------------------------------------");
+      System.out.println("Today Musical show" + " " +nameMusical);
+      System.out.println("         (1)Booking  (2)Exit       ");   
+    }
+    
+    
+    
+    public int Select(Scanner input){
+        for (int i = 0; i <= 99; i++) {
+            seat[i] = "[" + i + "]";
+        }
+        
+        while (true) {
+            
+          
+              
+                   
+                    choice = scr.nextInt();
+                    switch (choice) {
+                        case 1:
+                            int index;
+                            System.out.println("---------------------------------------------");
+                            System.out.println("           Number Of Ticket");
+                            noTicket = scr.nextInt();
+                            System.out.println("---------------------------------------------");
+
+                            for (int i = 0; i <= 99; i++) {
+                                if ((i == 10) || (i == 20) || (i == 30) || (i == 40) || (i == 50) || (i == 60) || (i == 70) || (i == 80) || (i == 90)) {
+                                    System.out.println("\n");
+                                }
+                                System.out.print(seat[i]); 
+                            }
+                            System.out.println("\n\n           Select Your Seat");
+                            for (int i = 0; i < noTicket; i++) {
+                                noSeat[i] = scr.nextInt();
+                                index = noSeat[i];
+                               
+                            }
+                            Receipt();
+                            
+                            break;
+                      
+                        case 2:
+                            System.out.println("Thankyou and Hope to see you again.");
+                            break;
+                        
+                    }
+               }
+    }  
+    
+    public void Receipt(){
+       System.out.println("---------------------------------------------");
+       System.out.println("                  THEATRE                    ");
+       System.out.println("              "+nameMusical+"                ");
+                            
+       for (int i = 0; i < noTicket; i++) {
+       System.out.println("              SEAT NO.:" + noSeat[i] + "              ");
+       }
+       System.out.println("           TOTAL AMOUNT:" + noTicket * 500 + "           ");
+       System.out.println("---------------------------------------------------------");
+                              
+    }
+}
